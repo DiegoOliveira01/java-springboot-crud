c4322b8aee6a4b19f41e05042066506b21bc97b5
DiegoOliveira01 <diegofontes.002@gmail.com>
2026-02-24 19:40:41 -0300

    Initial commit

---
+++ src/main/java/com/diego/gerenciamento_usuario/GerenciamentoUsuarioApplication.java
@@ -0,0 +1,13 @@
+package com.diego.gerenciamento_usuario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoUsuarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoUsuarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/diego/gerenciamento_usuario/GerenciamentoUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diego.gerenciamento_usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
